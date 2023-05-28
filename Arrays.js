979a805aece5a0ed1553fd8556daf60d84478f92
dawood <dawood.altai111@gmail.com>
2023-05-28 23:51:57 +0300

    Length

---
+++ Arrays.js
@@ -0,0 +1,17 @@
+// Array length
+// Array toString()
+// Array pop()
+// Array push()
+// Array shift()
+// Array unshift()
+// Array join()
+// Array delete()
+// Array concat()
+// Array flat()
+// Array splice()
+// Array slice()
+// The methods are listed in the order they appear in this tutorial page
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let size = fruits.length;
+console.log(size);
